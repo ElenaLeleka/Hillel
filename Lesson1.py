6702f6d3741ae437e433b4acc648bcf82302d196
ElenaLeleka <98187724+ElenaLeleka@users.noreply.github.com>
2022-01-22 16:13:48 +0200

    Create Lesson1.py
    Initial commit

---
+++ Lesson1.py
@@ -0,0 +1,15 @@
+#Задача: Создать две переменные first=10, second=30. Вывести на экран построчно результат математического взаимодействия (+, -, *, /, **) для этих чисел.
+#Разобраться как работают операторы % и //. Вывести на экран эти операции для переменных из пункта 2.
+
+first = 10
+second = 30
+
+print (first + second)
+print (first * second)
+print (first - second)
+print (first / second)
+print (first ** second)
+
+print (first // second)
+print (first % second)
+
